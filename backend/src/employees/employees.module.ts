c2f331be16c6541ca6704770af55d67b86552d3b
kriskilsby <151669187+kriskilsby@users.noreply.github.com>
2026-01-17 00:47:25 +0000

    Ensure employees.module.ts imports all the correct modules.

--- backend/src/employees/employees.module.ts
+++ backend/src/employees/employees.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './employee.entity';
 import { EmployeesController } from './employees.controller';
+import { Qualification } from '../qualifications/qualification.entity';
+import { EmployeeProjectExperience } from '../employee-project-experience/employee-project-experience.entity';
+import { Cpd } from '../cpd/cpd.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Employee])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Employee,
+      Qualification,
+      EmployeeProjectExperience,
+      Cpd
+    ]),
+  ],
   controllers: [EmployeesController],
+  providers: [],
 })
 export class EmployeesModule {}
